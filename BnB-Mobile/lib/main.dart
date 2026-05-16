1868564cf10e20a1bd6a91f9bf616421ed595992
Youssef <test@gmail.com>
2026-05-16 22:39:05 +0300

    style(mobile): remove debug banner from MaterialApp

--- BnB-Mobile/lib/main.dart
+++ BnB-Mobile/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'B&B',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorSchemeSeed: Colors.blue,
         useMaterial3: true,
